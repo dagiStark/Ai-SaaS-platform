8c4a6a668915937eec37af0d125d879a4823aa5d
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-11 08:28:28 +0300

    added sidebar routing nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { UserButton } from "@clerk/nextjs";
-import Sidebar from "@/components/shared/Sidebar";
+
+
 const Home = () => {
   return (
-    <div className="border-4 border-red-500">
-      <Sidebar />
+    <div className="">
     </div>
   );
 };
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,10 +1,13 @@
-'use client'
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { navLinks } from "@/constants";
 import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -13,7 +16,7 @@ const Sidebar = () => {
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
           <Image
-            src="/public/assets/images/logo-text.svg"
+            src="/assets/images/logo-text.svg"
             alt="logo"
             width={180}
             height={28}
@@ -23,7 +26,7 @@ const Sidebar = () => {
         <nav className="sidebar-nav">
           <SignedIn>
             <ul className="sidebar-nav-elements">
-              {navLinks.map(({ label, route, icon }) => {
+              {navLinks.slice(0, 6).map(({ label, route, icon }) => {
                 const isActive = route === pathname;
                 return (
                   <li
@@ -34,21 +37,56 @@ const Sidebar = () => {
                         : "text-gray-700"
                     }`}
                   >
-                    <Link className="sidebar-link" href={route}>
-                        <Image 
-                            src={icon}
-                            alt="logo"
-                            width={24}
-                            height={24}
-                            className={`${isActive} && 'brightness-200`}
-                        />
-                        {label}
+                    <Link className="sidebar-link cursor-pointer" href={route}>
+                      <Image
+                        src={icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive} && 'brightness-200`}
+                      />
+                      {label}
                     </Link>
                   </li>
                 );
               })}
             </ul>
+            <ul className="sidebar-nav-elements">
+              {navLinks.slice(6).map(({ label, route, icon }) => {
+                const isActive = route === pathname;
+                return (
+                  <li
+                    key={route}
+                    className={`sidebar-nav-element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link cursor-pointer" href={route}>
+                      <Image
+                        src={icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive} && 'brightness-200`}
+                      />
+                      {label}
+                    </Link>
+                  </li>
+                );
+              })}
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton showName />
+              </li>
+            </ul>
           </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </aside>
